ec2af1953f3ea0024c3bea3df346c6e562a0769e
mrefawaladam <aerfa55@gmail.com>
2022-02-22 20:55:41 +0700

    Tailwind and template settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss(
+
+ mix.js('resources/js/app.js', 'public/js').postCss(
     'resources/css/app.css',
-     'public/css', [
-        //
-    ]);
+    'public/css',
+    [require("tailwindcss")]
+);
+
 
 mix.browserSync("127.0.0.1:8000")
